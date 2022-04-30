66a731bcb8778235267bb955613031d81515e7aa
esraa ahmed <esraa.ahmed@bgomla.com>
2022-04-30 15:35:58 +0200

    Add project files.

---
+++ ObjectFiller/Models/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectFiller.Models
+{
+    public record StudentModel
+    {
+        public Guid StudentId { get; set; }
+        public string StudentName { get; set;}
+        public int StudentAge { get; set;}
+        public string StudentEmail { get; set; }
+        public string StudentPhone { get; set; }
+    }
+}
---
+++ ObjectFiller/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using ObjectFiller.Models;
+using System;
+using System.Collections.Generic;
+using Tynamix.ObjectFiller;
+
+//Console.WriteLine("Hello, World!");
+
+    String num = "0123456789";
+    int length = 10;
+    var random = new Random();
+    var filler = new Filler<StudentModel>();
+    filler.Setup()
+        .OnProperty(x => x.StudentId).Use(Guid.NewGuid())
+        .OnProperty(x => x.StudentName).Use(new RealNames(NameStyle.FirstNameLastName))
+        .OnProperty(x => x.StudentAge).Use(Enumerable.Range(5, 25))
+        .OnProperty(x => x.StudentEmail).Use(new EmailAddresses())
+        .OnProperty(x => x.StudentPhone).Use(new string(Enumerable.Repeat(num,length)
+                                    .Select(x => x[random.Next(x.Length)]).ToArray()));
+
+    IEnumerable<StudentModel> Students = filler.Create(5);
+    foreach (var Student in Students)
+    {
+       Console.WriteLine("Student Id: " + Student.StudentId);
+       Console.WriteLine("Student Name: " + Student.StudentName);
+       Console.WriteLine("Student Age: " + Student.StudentAge);
+       Console.WriteLine("Student Email: "+ Student.StudentEmail);
+       Console.WriteLine("Student Phone: "+"0"+ Student.StudentPhone);
+       Console.ReadLine();
+    }
+
+//Console.WriteLine(Students[1]);
+
